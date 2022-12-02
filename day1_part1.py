76c44ea8d63551675a94103c1f8644ca0a92fc4e
Harma Everts <113139633+HarmaEverts@users.noreply.github.com>
2022-12-02 10:38:06 +0100

    Added day 1

---
+++ day1_part1.py
@@ -0,0 +1,16 @@
+import os
+
+max_calories = 0
+
+with open("inputday1.txt", 'r') as f:
+    lines = f.read().splitlines()
+    temp_calories = 0
+    for line in lines:
+        if line != '': # found an elf's calories
+            temp_calories += int(line)
+        else: # found the end of what an elf carries
+            if temp_calories > max_calories:
+                max_calories = temp_calories
+            temp_calories = 0
+
+print(max_calories)
---
+++ day1_part2.py
@@ -0,0 +1,17 @@
+import os
+
+elves_inventory = []
+
+with open("inputday1.txt", 'r') as f:
+    lines = f.read().splitlines()
+    temp_calories = 0
+    for line in lines:
+        if line != '': # found an elf's calories
+            temp_calories += int(line)
+        else: # found the end of what an elf carries
+            elves_inventory.append(temp_calories)
+            temp_calories = 0
+
+elves_inventory.sort(reverse=True)
+
+print(elves_inventory[0] + elves_inventory[1] + elves_inventory[2])
