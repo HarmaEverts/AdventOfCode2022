9a6af640813726afcdf0c6314fb787a33e7772a7
Harma Everts <82085008+Meilindis@users.noreply.github.com>
2022-12-06 20:04:00 +0100

    Day 6 both puzzles.

---
+++ Day 6/day6_part1.py
@@ -0,0 +1,19 @@
+def check_for_duplicate(signal):
+    for item in signal:
+        if signal.count(item) > 1:
+            return True
+    return False
+
+
+def find_marker(stream):
+    temp_set = []
+    for i in enumerate(stream):
+        temp_set = stream[i[0]:i[0]+4]
+        if not check_for_duplicate(temp_set):
+            return i[0]+4
+
+
+with open('day6_input.txt', 'r') as f:
+    message = f.readline()
+    marker = find_marker(message)
+    print(marker)
